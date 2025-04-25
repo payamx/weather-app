a550049c3b797e292681e5f0c07edf363ef67148
payamx <moblerezaei3@gamil.com>
2025-04-25 18:50:07 +0330

    rain drop

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], base:"/weather-app"
+  plugins: [react()], base:"/weather-app/"
 })
